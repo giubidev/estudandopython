b7b9f5968251a4e99b8b9e3924e111738c4a7e3f
giubidev <135030402+giubidev@users.noreply.github.com>
2023-05-30 13:33:25 -0300

    Create ex3.py

---
+++ Estrutura Sequencial/ex3.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+soma = num1 + num2
+
+print("A soma dos dois números é", soma)
