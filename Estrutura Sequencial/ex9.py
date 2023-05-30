cd3518c038668aa153e3520669c42ac8123ae81b
giubidev <135030402+giubidev@users.noreply.github.com>
2023-05-30 14:20:47 -0300

    Create ex9.py

---
+++ Estrutura Sequencial/ex9.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+fahr = float(input("Digite a temperatura em graus Fahrenheit: "))
+
+celsius = 5 * ((fahr-32) / 9)
+
+print(celsius)
