2db04e3f46212aa3c48095becb5e062ba4b21c37
giubidev <135030402+giubidev@users.noreply.github.com>
2023-05-30 13:46:47 -0300

    Create ex5.py

---
+++ Estrutura Sequencial/ex5.py
@@ -0,0 +1,7 @@
+# Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Digite a medida em metros: "))
+
+conv = metros * 100
+
+print("A medida em centímetros é de ", conv ," cm")
