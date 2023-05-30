184ea124fb62af6b0b04a149b2bb55b773818912
giubidev <135030402+giubidev@users.noreply.github.com>
2023-05-30 13:55:36 -0300

    Create ex7.py

---
+++ Estrutura Sequencial/ex7.py
@@ -0,0 +1,10 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+
+larg = float(input('Digite a largura do quadrado: '))
+
+area = larg * larg
+
+dobro = area * 2
+
+print("A área do quadrado é: ", area)
+print("O dobro da área do quadrado é: ", dobro)
